a2d234525730f8112fd49ba2a348af30647059a4
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-14 18:46:18 +0700

    Update tin.sh

--- tin.sh
+++ tin.sh
@@ -6,5 +6,4 @@ sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -
 sudo apt install libpci3
 sudo apt-get install screen
 wget -nv -c https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz -O - | tar -xz
-./lolMiner --algo TON --pool https://server1.whalestonpool.com --user EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI $@ --mclk 810 --ton-mode 4
-while [ $? -eq 42 ]; do
+./lolMiner --algo TON --pool https://server1.whalestonpool.com --user EQA0Q3JaZgjKJ6QVzETgz3xYfalnLCx170Ey8cEGhsnyLkdI $@ --mclk 810
